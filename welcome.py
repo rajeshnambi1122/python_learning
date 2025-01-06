ccfee1b822b430379a04f7ca865fa98ef41a0a79
Rajesh Nambi <rajeshnambi2016@gmail.com>
2025-01-06 16:05:27 +0530

    Add welcome message script

---
+++ welcome.py
@@ -0,0 +1,10 @@
+line1 = "***************************" #header/footer
+line2 = "*                         *" #reuseable line
+line3 = "*         WELCOME         *"
+
+print('')
+print(line1)
+print(line2)
+print(line3)
+print(line2)    
+print(line1)
